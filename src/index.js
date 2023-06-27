38e8ab36f09cd148a82105e1c7c6a4b6d5f3bf7a
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2023-06-27 03:16:26 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
-const fs = require("fs");
-const path = require("path");
+const { promises: fsPromises, createReadStream } = require("fs");
+const { join } = require("path");
 const express = require("express");
+
 
 const PORT = 3002;
 const app = express();
-const videoPath = path.join(__dirname, "../videos/SampleVideo_1280x720_1mb.mp4");
+const videoPath = join(__dirname, "../videos/SampleVideo_1280x720_1mb.mp4");
 
-app.get("/video", async (req, res) => {
-  try {
-    const stats = await fs.promises.stat(videoPath);
 
-    res.writeHead(200, {
-      "Content-Length": stats.size,
+///////////////MAIN//////////////////
+app.get("/video", serveVideo);
+startServer();
+/////////////////////////////////////
+
+
+
+function startServer() {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+
+
+  async function serveVideo(req, res) {
+    try {
+      const stats = await getFileStats(videoPath);
+      setResponseHeaders(res, stats.size);
+      const videoStream = createReadStream(videoPath);
+      videoStream.pipe(res);
+    } catch (error) {
+      handleServerError(res, error);
+    }
+  }
+  async function getFileStats(filePath) {
+    return fsPromises.stat(filePath);
+  }
+
+  function setResponseHeaders(res, fileSize) {
+    res.set({
+      "Content-Length": fileSize,
       "Content-Type": "video/mp4",
     });
+  }
 
-    const videoStream = fs.createReadStream(videoPath);
-    videoStream.pipe(res);
-  } catch (error) {
+function handleServerError(res, error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
-  }
-});
+  }  
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  
+ 
+  
+ 
