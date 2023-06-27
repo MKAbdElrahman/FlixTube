d9de3c30887cdddd6602d021a37f0c353f8a0c55
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2023-06-27 20:59:49 +0300

    configureing port number of the vidoe streaming microservice

--- src/index.js
+++ src/index.js
@@ -3,24 +3,30 @@ const { join } = require("path");
 const express = require("express");
 
 
-const PORT = 3002;
 const app = express();
 const videoPath = join(__dirname, "../videos/SampleVideo_1280x720_1mb.mp4");
 
 
+
 ///////////////MAIN//////////////////
+const PORT = getPortNumber();
 app.get("/video", serveVideo);
-startServer();
+startServer(PORT);
 /////////////////////////////////////
 
-
-
-function startServer() {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+function getPortNumber() {
+  if (!process.env.PORT) {
+    throw new Error(`Please specify the port number for the HTTP server with the environment variable PORT.`);
   }
+  return process.env.PORT;
+}
 
+
+function startServer(portNumber) {
+    app.listen(portNumber, () => {
+      console.log(`Server is running on port ${portNumber}`);
+    });
+}
 
   async function serveVideo(req, res) {
     try {
